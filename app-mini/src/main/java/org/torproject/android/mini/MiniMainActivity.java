02bd7a1ea699384d353fdd8f859144c848cb1fb9
root <root@ip-172-31-78-11.ec2.internal>
2020-11-13 18:57:09 +0000

    Fix Null Dereference Check
    In file: MiniMainActivity.java, class: MiniMainActivity, method: updateStatus there is a potential Null pointer dereference. This may throw an unexpected null pointer exception which, if unhandled, may crash the program. iCR detected the null pointer issue and demonstrated the full path from the object declaration to the null dereference in the object. A developer should introduce null checks in the appropriate path or initialize the object explicitly.

--- app-mini/src/main/java/org/torproject/android/mini/MiniMainActivity.java
+++ app-mini/src/main/java/org/torproject/android/mini/MiniMainActivity.java
@@ -788,7 +788,18 @@ public class MiniMainActivity extends AppCompatActivity
 
         if (!TextUtils.isEmpty(torServiceMsg))
         {
-            if (torServiceMsg.contains(TorServiceConstants.LOG_NOTICE_HEADER)) {
+            /* ********OpenRefactory Warning********
+			 Possible null pointer Dereference!
+			 Path: 
+				File: MiniMainActivity.java, Line: 526
+					updateStatus(null,torStatus);
+					 Information is passed through the method call via null to the formal param torServiceMsg of the method. This later results into a null pointer dereference.
+				File: MiniMainActivity.java, Line: 791
+					torServiceMsg.contains(TorServiceConstants.LOG_NOTICE_HEADER)
+					torServiceMsg is referenced in method invocation.
+					The expression is enclosed inside an If statement.
+			*/
+			if (torServiceMsg.contains(TorServiceConstants.LOG_NOTICE_HEADER)) {
            //     lblStatus.setText(torServiceMsg);
             }
 
